771589b0361a4fcfac18d4e1c15644dbe6843d23
Justin <mjl6005@gmail.com>
2020-04-06 21:28:41 +0900

    cookie module add

--- nodejs-cookie/cookie.js
+++ nodejs-cookie/cookie.js
@@ -1,8 +1,22 @@
+// 쿠키설치
+// >>npm install -s cookie
+
+
 var http = require('http');
+var cookie = require('cookie');
 var port = 3000;
+
+
 var server = http.createServer(function(req,res){
 
     console.log(req.headers.cookie);
+    var cookies = {};
+    if(req.headers.cookie != undefined){
+        cookies = cookie.parse(req.headers.cookie);
+    }
+    console.log(cookies);
+    console.log(cookies.num);
+    
 
     res.writeHead(200,{
         'Content-Type':'text/html; charset=UTF-8',
